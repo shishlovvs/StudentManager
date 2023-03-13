dbdbfb19e16505b7ba0d5f9bf49b911ebec7ff4b
shishlovvs <shishlovvs@mail.ru>
2023-03-13 21:13:20 +0400

    Добавьте файлы проекта.

---
+++ StudentManager/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Controls;
+using System.Diagnostics.Eventing.Reader;
+
+namespace StudentManager
+{
+    internal class AppDbContext : DbContext
+    {
+        private const string ConnectionString = "Data Source = hello.db";
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+
+        public DbSet<Student> Students => Set<Student>();
+    }
+
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ StudentManager/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+           await using var db = new AppDbContext();
+           await db.Database.EnsureCreatedAsync(); //Проверяет, есть ли таблица в БД7
+            db.Students.AddAsync(new Student() { 
+                Name = "Иммануил Кант", 
+                Birthday = DateTime.Now 
+            });
+            db.Students.AddAsync(new Student()
+            {
+                Name = "Лев Толстой",
+                Birthday = DateTime.Today.AddDays(-1)
+            });
+            await db.SaveChangesAsync();
+
+            var students = await db.Students.ToListAsync();
+            studentsDataGrid.ItemsSource = students;
+        }
+    }
+}
